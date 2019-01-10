e1e4b021279ac59737208874f0e579b1c672f753
Hinken <henkrist96@gmail.com>
2019-01-10 11:02:16 +0100

    edits

--- js/scripts.js
+++ js/scripts.js
@@ -71,35 +71,6 @@ $(document).ready(function(){
 	}
 });
 
-
-// andre forsøk
-
-/*
-var target = this.hash; // gets the #hash
-$target = $(target); //
-$('html, body').stop().animate({
-    'scrollTop': $target.offset().top+2 // scrolls to the link
-}, 500, 'swing', function () {
-    window.location.hash = target;
-    $(document).on("scroll", onScroll);
-});
-
-function onScroll(event){
-    var scrollPosition = $(document).scrollTop();
-    $('ul li a').each(function () {
-        var currentLink = $(this);
-        var refElement = $(currentLink.attr("href"));
-        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-            $('ul liv a.current').removeClass('current');
-            $(this).addClass('current');
-        }
-        else{
-            currentLink.removeClass("current");
-        }
-    });
-}*/
-
-
 // map
 function initMap() {
     var ipd = {lat: 63.418143, lng: 10.404537};
